0127b71a2f01d492758abcaf7119193295b0e79b
Micah Nordland <micah@rehack.me>
2015-04-06 16:57:22 -0400

    Move models and routes into separate posts package expanded config options

--- main.go
+++ main.go
@@ -1,46 +1,54 @@
 package main
 
 import (
+	"github.com/astaxie/beego/orm"
 	"github.com/gorilla/mux"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/spf13/viper"
+    "gopkg.in/flosch/pongo2.v3"
 	"net/http"
-    "github.com/spf13/viper"
-    _ "github.com/mattn/go-sqlite3"
-    "github.com/astaxie/beego/orm"
+    "fmt"
 )
 
 func init() {
-    //Config
-    //Set Default values
-    viper.SetDefault("port", "8080")
-    viper.SetDefault("proto", "http")
-    viper.SetDefault("staticDir", "./static/")
-    viper.SetDefault("templateDir", "./template/")
-    //Read in config file
-    viper.SetConfigName("config")
-    viper.AddConfigPath("/etc/blgr/")
-    viper.AddConfigPath("$HOME/.blgr")
-    viper.AddConfigPath(".")
-    viper.ReadInConfig()
-    //Allow flag or environment overrides
-    viper.SetEnvPrefix("BLGR")
-    viper.AutomaticEnv()
+	//Config
+	//Set Default values
+	viper.SetDefault("port", "8080")
+	viper.SetDefault("proto", "http")
+	viper.SetDefault("staticDir", "./static/")
+	viper.SetDefault("templateDir", "./templates/")
+	//Read in config file
+	viper.SetConfigName("config")
+	viper.AddConfigPath("/etc/blgr/")
+	viper.AddConfigPath("$HOME/.blgr")
+	viper.AddConfigPath(".")
+	viper.ReadInConfig()
+	//Allow flag or environment overrides
+	viper.SetEnvPrefix("BLGR")
+	viper.AutomaticEnv()
 
-    //Database
-    orm.RegisterDataBase("default", "sqlite3", "./blgr.db") 
+	//Database
+	orm.RegisterDataBase("default", "sqlite3", "./blgr.db")
+
+    //Template setup
+    err := pongo2.DefaultSet.SetBaseDirectory(viper.GetString("templateDir"))
+    if err != nil {
+        fmt.Println("Error setting template base dir:", err.Error())
+    }
 }
 
 func main() {
-    //Create the router
+	//Create the router
 	rtr := mux.NewRouter()
+	rtr.StrictSlash(true)
+	//Connect our routes in with the router
+	connectRoutes(rtr)
 
-    //Connect our routes in with the router
-    connectRoutes(rtr)
-
-    //Hook the router up to the server
+	//Hook the router up to the server
 	http.Handle("/", rtr)
-    
-    //Serve it to the world!
-    port := viper.GetString("port")
+
+	//Serve it to the world!
+	port := viper.GetString("port")
 	http.ListenAndServe(":"+port, nil)
 
 }
--- routes.go
+++ routes.go
@@ -1,33 +1,30 @@
 package main
 
 import (
-    "github.com/gorilla/mux"
-    "net/http"
+	"github.com/gorilla/mux"
+    "github.com/mpnordland/blgr/posts"
+	"net/http"
 )
 
-//Here's where we define the routes for 
+//Here's where we define the routes for
 //the application
 func connectRoutes(router *mux.Router) {
-    //Create subrouters for these paths
-    feedR := router.PathPrefix("/feed/").Subrouter()
-    postR := router.PathPrefix("/posts").Subrouter()
+	//Create subrouters for these paths
+	feedR := router.PathPrefix("/feed/").Subrouter()
+	postR := router.PathPrefix("/posts").Subrouter()
 
-    //Front page
-    router.Handle("/", http.HandlerFunc(FrontView))
+	//Front page
+	router.Handle("/", http.HandlerFunc(FrontView))
 
-    //Connect routes
-    feedRoutes(feedR)
-    postRoutes(postR)
+	//Connect routes
+	feedRoutes(feedR)
+	posts.SetRoutes(postR)
 
-    //Serve static files
-    router.Handle("/static/{file}", http.HandlerFunc(staticHandler))
+	//Serve static files
+	router.HandleFunc("/static/{file:.+}", staticHandler)
 
 }
 
 func feedRoutes(r *mux.Router) {
-}
-
-func postRoutes(r *mux.Router) {
-    r.HandleFunc("/add/", AddPostView)    
 }
 
