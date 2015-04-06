c47858ce353c3a32aed8536b10c1a0188e7526c3
Micah Nordland <micah@rehack.me>
2015-04-06 16:56:36 -0400

    Added basic assets and improved static file handling

--- static.go
+++ static.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-    "net/http"
+	"github.com/spf13/viper"
     "github.com/gorilla/mux"
-    "github.com/spf13/viper"
+	"net/http"
 )
 
 func staticHandler(w http.ResponseWriter, r *http.Request) {
     vars := mux.Vars(r)
-    staticDir := viper.GetString("staticDir")
+	staticDir := viper.GetString("staticDir") 
     http.ServeFile(w, r, staticDir+vars["file"])
 }
